df1452f1addeadf4ace395522344b31f9a7b356a
victor <victor.mendes@cbpf.br>
2025-08-13 09:46:42 -0300

    ✨ Ajuste na altura da footer

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -10,8 +10,8 @@ const year = new Date().getFullYear();
 </script>
 
 <template>
-    <footer :class="loadMobileNav ? 'pb-24 pt-6' : 'py-6'" class="bg-[#0B1120] text-slate-300">
+    <footer :class="loadMobileNav ? 'pb-6 pt-6' : 'py-6'" class="bg-slate-200 text-[#0B1120] dark:bg-[#0B1120] dark:text-slate-300">
         <p class="text-center text-sm md:text-base"> {{ year }} &copy; {{ t('footer.designedBy') }}
-        <a target="_blank" href="https://github.com/VictorMendesON" class="text-secondary-400 font-medium underline underline-offset-4">Victor Mendes</a></p>
+        <a target="_blank" href="https://github.com/VictorMendesON" class="text-primary-600 font-semibold dark:text-secondary-400  underline underline-offset-4">Victor Mendes</a></p>
     </footer>
 </template>
