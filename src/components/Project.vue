876d1f76eff080c9248e3e94ec8b852ccdafc3bb
victor <victor.mendes@cbpf.br>
2025-10-01 11:32:55 -0300

    ✨ Add new project personal

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,5 +1,5 @@
 <script setup>
-const props = defineProps(["image", "category", "title", "tags", "codeLink", "liveLink", "caseStudy"]);
+const props = defineProps(["image", "category", "title", "tags", "codeLink"]);
 </script>
 
 <template>
@@ -25,7 +25,7 @@ const props = defineProps(["image", "category", "title", "tags", "codeLink", "li
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
                     </svg>
                 </a>
-                <a target="_blank" v-if="props.liveLink" :href="liveLink" class="transition-all duration-300 hover:text-secondary-500 dark:hover:text-secondary-400">
+                <!-- <a target="_blank" v-if="props.liveLink" :href="liveLink" class="transition-all duration-300 hover:text-secondary-500 dark:hover:text-secondary-400">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                     </svg>
@@ -35,7 +35,7 @@ const props = defineProps(["image", "category", "title", "tags", "codeLink", "li
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                     </svg>
-                </a>
+                </a> -->
             </div>
         </div>
     </div>
